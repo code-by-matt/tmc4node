2d1186ff3f4e49b3eae657d1caa5abdf69df0a61
Matt Yuan <matthewfelixyuan@gmail.com>
2019-07-22 17:38:39 -0400

    tautological test now makes a get request and returns the whole body. will truncate that body later

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,9 @@ const request = require("request");
 
 describe("Routing.", function() {
   it("Should be truthy.", function() {
+    request("http://localhost:8000", function(error, response, body) {
+      console.log(body);
+    });
     expect(true).to.equal(true);
   });
 });
