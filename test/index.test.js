d8e1ed5cfb081566b31440951d3af5face7d8b36
Matt Yuan <matthewfelixyuan@gmail.com>
2019-07-24 10:22:41 -0400

    test that the first player can connect

--- test/index.test.js
+++ test/index.test.js
@@ -6,9 +6,18 @@ const {JSDOM} = jsdom;
 describe("Routing.", function() {
   it("Should display the welcome page.", function(done) {
     request.get("http://localhost:8000", function(error, response, body) {
-      var dom = new JSDOM(body, {runScripts: "dangerously"});
+      var dom = new JSDOM(body);
       var title = dom.window.document.querySelector("title").textContent;
       expect(title).to.equal("TMC4 | Welcome!");
+      done();
+    });
+  });
+  it("Should display a game for the first player.", function(done) {
+    var id = Math.random().toString(36).substr(6);
+    request.get("http://localhost:8000/game?id=" + id, function(error, response, body) {
+      var dom = new JSDOM(body, {url: "http://localhost:8000", runScripts: "dangerously", resources: "usable"});
+      var title = dom.window.document.querySelector("title").textContent;
+      expect(title).to.equal("TMC4 | Play!");
       done();
     });
   });
