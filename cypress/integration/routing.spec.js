a36aa3a375e24c787443794e63319d8b9b4e1a70
Matt Yuan <matthewfelixyuan@gmail.com>
2019-07-27 14:57:22 -0400

    write second test. so easy. mwah, chef's kiss

--- cypress/integration/routing.spec.js
+++ cypress/integration/routing.spec.js
@@ -1,8 +1,16 @@
 /// <reference types="Cypress" />
+var io = require("socket.io-client");
 
 describe("Routing.", function() {
+
   it("Should display the welcome page.", function() {
     cy.visit("http://localhost:8000");
     cy.get("title").should("have.text", "TMC4 | Welcome!");
+  });
+
+  it("Should display a game for the first player.", function() {
+    var id = Math.random().toString(36).substr(6);
+    cy.visit("http://localhost:8000/game?id=" + id);
+    cy.get("title").should("have.text", "TMC4 | Play!");
   });
 });
