6d33b2ca2f1362c4fe4ab0967892fd634cf0de58
Matt Yuan <matthewfelixyuan@gmail.com>
2019-07-28 14:03:24 -0400

    HUZZAH CYPRESS TASKS WORK

--- cypress/integration/routing.spec.js
+++ cypress/integration/routing.spec.js
@@ -1,5 +1,4 @@
 /// <reference types="Cypress" />
-var io = require("socket.io-client");
 
 describe("Routing.", function() {
 
@@ -16,25 +15,15 @@ describe("Routing.", function() {
 
   it("Should display a game for the second player.", function() {
     var id = Math.random().toString(36).substr(6);
-    var socket = io("http://localhost:8000");
-    socket.emit("join room", id);
-    
-      cy.visit("http://localhost:8000/game?id=" + id);
-      cy.get("title").should("have.text", "TMC4 | Play!");
-    
+    cy.task("one", id);
+    cy.visit("http://localhost:8000/game?id=" + id);
+    cy.get("title").should("have.text", "TMC4 | Play!");
   });
 
   it("Should NOT display a game for the third player.", function() {
     var id = Math.random().toString(36).substr(6);
-    var socket1 = io("http://localhost:8000");
-    socket1.emit("join room", id);
-    socket1.on("room joined", function() {
-      var socket2 = io("http://localhost:8000");
-      socket2.emit("join room", id);
-      socket2.on("room joined", function() {
-        cy.visit("http://localhost:8000/game?id=" + id);
-        cy.get("title").should("have.text", "TMC4 | Game Not Found!");
-      });
-    });
+    cy.task("two", id);
+    cy.visit("http://localhost:8000/game?id=" + id);
+    cy.get("title").should("have.text", "TMC4 | Game Not Found!");
   });
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,38 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+var client = require("socket.io-client");
+
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+
+  on("task", {
+
+    // Add one socket to the room with the given id.
+    one(id) {
+      return new Promise(function(resolve) {
+        var socket = client("http://localhost:8000");
+        socket.emit("join room", id);
+        socket.on("room joined", function() {
+          resolve(null);
+        });
+      });
+    },
+
+    // Add two sockets to the room with the given id.
+    two(id) {
+      return new Promise(function(resolve) {
+        var socket1 = client("http://localhost:8000");
+        socket1.emit("join room", id);
+        socket1.on("room joined", function() {
+          var socket2 = client("http://localhost:8000");
+          socket2.emit("join room", id);
+          socket2.on("room joined", function() {
+            resolve(null);
+          });
+        });
+      });
+    }
+  });
+};
