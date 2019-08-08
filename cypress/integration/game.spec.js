a82bf24d3cdad9cf25473419e8e762bea4248d41
Matt Yuan <matthewfelixyuan@gmail.com>
2019-08-08 13:37:59 -0400

    improve gaemplay test

--- cypress/integration/game.spec.js
+++ cypress/integration/game.spec.js
@@ -42,9 +42,11 @@ describe("Gameplay.", function() {
       .pause();
   });
 
-  it("Should play a game.", function() {
+  it("Should play a game with connection", function() {
     var id = Math.random().toString(36).substr(6);
     cy.visit("http://localhost:8000/game?id=" + id);
+
+    // Check that interacting with the start panel emits the right things.
     cy.get("#start-panel .my-name")
       .type("BoJack{enter}");
     cy.get('[for="thr-min"]')
@@ -55,6 +57,8 @@ describe("Gameplay.", function() {
       .log("message three minutes?")
       .log("message ready?")
       .pause();
+
+    // Check that we can receive the opponent's start panel activity.
     cy.task("my", {type: "sender name", thing: "Carolyn", id: id});
     cy.task("my", {type: "message", thing: "ten minutes", id: id});
     cy.task("my", {type: "message", thing: "ready", id: id});
@@ -64,6 +68,8 @@ describe("Gameplay.", function() {
       .should("be.checked");
     cy.get("#ready")
       .should("not.be.checked");
+
+    // Check that the game starts and is emitted when all things are prepared.
     cy.get('[for="ready"]')
       .click()
       .log("message transfer names?")
@@ -75,14 +81,20 @@ describe("Gameplay.", function() {
       .should("not.be.visible");
     cy.get("#play-panel")
       .should("not.be.visible");
+
+    // Force our player to be red and feed in a dummy game, which also checks that we can receive a game.
     cy.task("my", {type: "message", thing: "sender is blue", id: id});
     var stats = {
       history: "r30r31r32",
       openRows: [0, 0, 0, 3, 0, 0, 0],
     };
     cy.task("my", {type: "game stats", thing: stats, id: id});
-    cy.get("#board-img").click(200, 25);
+
+    // Make a winning move to check that the game ends.
+    cy.get("#board-img")
+      .click(200, 25);
     cy.get("#end-panel")
-      .should("be.visible");
+      .should("be.visible")
+      .should("have.text", "Red wins by connection!");
   });
 });
