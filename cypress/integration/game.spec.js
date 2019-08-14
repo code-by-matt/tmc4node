47cbcbeb9e14fd897ef1a9f6892af3de3354f485
Matt Yuan <matthewfelixyuan@gmail.com>
2019-08-14 13:21:15 -0400

    create the modify-send test

--- cypress/integration/game.spec.js
+++ cypress/integration/game.spec.js
@@ -35,6 +35,8 @@ describe("Routing.", function() {
 
 describe("Gameplay.", function() {
 
+  // // Note that certain received stats will be flipped, but our sync task does NOT flip any stats!
+
   it("Should look nice on an iPhone 5.", function() {
     cy.viewport("iphone-5");
     var id = Math.random().toString(36).substr(6);
@@ -55,6 +57,9 @@ describe("Gameplay.", function() {
       iAmReady: false,
       myName: "Carolyn",
       timeControl: 10,
+      iAmRed: null,
+      theyWantMore: false,
+      iWantMore: false,
     }, id: id});
     cy.get("#start")
       .should("be.visible");
@@ -89,6 +94,8 @@ describe("Gameplay.", function() {
       moveStart: new Date().getTime(),
       redTime: 567000,
       bluTime: 587500,
+      theyWantMore: false,
+      iWantMore: false,
     }, id: id});
     cy.get("#start")
       .should("not.be.visible");
@@ -128,6 +135,8 @@ describe("Gameplay.", function() {
       bluTime: 587500,
       winner: "Blue",
       winBy: "connection",
+      theyWantMore: false,
+      iWantMore: false,
     }, id: id});
     cy.get("#start")
       .should("not.be.visible");
@@ -148,6 +157,151 @@ describe("Gameplay.", function() {
     cy.get("#end")
       .should("have.text", "Blue wins by connection!")
       .pause();
+  });
+
+  it("Should modify and send games correctly.", function() {
+    var id = Math.random().toString(36).substr(6);
+    cy.visit("http://localhost:8000/game?id=" + id);
+    cy.get(".row-center").eq(0)
+      .should("have.text", "Connected!");
+
+    // Check initial stats.
+    cy.task("sync", id)
+      .should("deep.equal", {
+        theyAreReady: false,
+        theirName: null,
+        iAmReady: false,
+        myName: null,
+        timeControl: null,
+        currentTurn: null,
+        firstTurn: null,
+        openRows: null,
+        history: null,
+        future: null,
+        iAmRed: null,
+        moveStart: null,
+        redTime: null,
+        bluTime: null,
+        winner: null,
+        winBy: null,
+        theyWantMore: false,
+        iWantMore: false,
+      }
+    );
+
+    // Check stats after entering a name.
+    cy.get("#start .name").eq(0)
+      .type("Diane{enter}");
+    cy.task("sync", id)
+      .should("deep.equal", {
+        theyAreReady: false,
+        theirName: null,
+        iAmReady: false,
+        myName: "Diane",
+        timeControl: null,
+        currentTurn: null,
+        firstTurn: null,
+        openRows: null,
+        history: null,
+        future: null,
+        iAmRed: null,
+        moveStart: null,
+        redTime: null,
+        bluTime: null,
+        winner: null,
+        winBy: null,
+        theyWantMore: false,
+        iWantMore: false,
+      }
+    );
+
+    // Check stats after choosing a time control.
+    cy.get('[for="thr-min"]')
+      .click();
+    cy.task("sync", id)
+      .should("deep.equal", {
+        theyAreReady: false,
+        theirName: null,
+        iAmReady: false,
+        myName: "Diane",
+        timeControl: 3,
+        currentTurn: null,
+        firstTurn: null,
+        openRows: null,
+        history: null,
+        future: null,
+        iAmRed: null,
+        moveStart: null,
+        redTime: null,
+        bluTime: null,
+        winner: null,
+        winBy: null,
+        theyWantMore: false,
+        iWantMore: false,
+      }
+    );
+
+    // Check stats after clicking ready.
+    cy.get('[for="ready"]')
+      .click();
+    cy.task("sync", id)
+      .should("deep.equal", {
+        theyAreReady: false,
+        theirName: null,
+        iAmReady: true,
+        myName: "Diane",
+        timeControl: 3,
+        currentTurn: null,
+        firstTurn: null,
+        openRows: null,
+        history: null,
+        future: null,
+        iAmRed: null,
+        moveStart: null,
+        redTime: null,
+        bluTime: null,
+        winner: null,
+        winBy: null,
+        theyWantMore: false,
+        iWantMore: false,
+      }
+    );
+
+    // Check play animation and stats after starting a game.
+    cy.task("my", {type: "game stats", thing: {
+      theyAreReady: false,
+      theirName: "Diane",
+      iAmReady: true,
+      myName: "Peanutbutter",
+      timeControl: 10,
+      iAmRed: null,
+      theyWantMore: false,
+      iWantMore: false,
+    }, id: id});
+    cy.get('[for="ready"]')
+      .click();
+    cy.get("#play")
+      .should("be.visible");
+    cy.task("sync", id).then(function(stats) {
+      cy.wrap(stats).its("theyAreReady").should("be.true");
+      cy.wrap(stats).its("theirName").should("equal", "Peanutbutter");
+      cy.wrap(stats).its("iAmReady").should("be.true");
+      cy.wrap(stats).its("myName").should("equal", "Diane");
+      cy.wrap(stats).its("timeControl").should("equal", 10);
+      cy.wrap(stats).its("currentTurn").should("not.be.null");
+      cy.wrap(stats).its("firstTurn").should("not.be.null");
+      cy.wrap(stats).its("openRows").should("not.be.null");
+      cy.wrap(stats).its("history").should("not.be.null");
+      cy.wrap(stats).its("future").should("not.be.null");
+      cy.wrap(stats).its("iAmRed").should("not.be.null");
+      cy.wrap(stats).its("moveStart").should("not.be.null");
+      cy.wrap(stats).its("redTime").should("equal", 600000);
+      cy.wrap(stats).its("bluTime").should("equal", 600000);
+      cy.wrap(stats).its("winner").should("be.null");
+      cy.wrap(stats).its("winBy").should("be.null");
+      cy.wrap(stats).its("theyWantMore").should("be.false");
+      cy.wrap(stats).its("iWantMore").should("be.false");
+    });
   });
 
   // it("Should play a game with connection.", function() {
